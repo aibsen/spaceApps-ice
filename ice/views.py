2be6a97a956ea13853821b302027e75c9a0bfa34
zeeerooo <boris.rq@gmail.com>
2017-04-29 17:54:04 -0300

    update date in map url

--- ice/views.py
+++ ice/views.py
@@ -3,13 +3,8 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse
-import h5py
+
 def index(request):
-    fileName = "ice/data/ATM_dense_point_cloud/2013.03.20/ILATM1B_20130320_141441.ATM4BT4.h5"
-    f = h5py.File(fileName,  "r")
-    for item in f.attrs.keys():
-        print item + ":", f.attrs[item]
-    f.close()
     return render(request, 'index.html')
 
 
